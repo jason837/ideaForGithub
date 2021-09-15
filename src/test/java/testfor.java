829b5c6b2ace599e2801a75a98fc84d91c621b67
jasonch <2538684421@qq.com>
2021-09-16 00:04:12 +0800

    testing the conflicts master and the hotfix

--- src/test/java/testfor.java
+++ src/test/java/testfor.java
@@ -2,5 +2,6 @@ public class testfor {
     public static void main(String[] args) {
         System.out.println("here commit soon");
         System.out.println("modified");
+        System.out.println("conflicts master;");
     }
 }
