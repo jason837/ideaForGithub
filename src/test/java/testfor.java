63dd48060dcea80068ab604d559d80667a44942a
jasonch <2538684421@qq.com>
2021-09-16 10:19:58 +0800

    add the Github-key for signing in;

--- src/test/java/testfor.java
+++ src/test/java/testfor.java
@@ -1,5 +1,6 @@
 public class testfor {
     public static void main(String[] args) {
+        // the github key: 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
         System.out.println("here commit soon");
         System.out.println("modified");
         System.out.println("conflicts master;");
